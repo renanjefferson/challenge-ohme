3c6545e4c0c525b5e926a8543a52638fa0bd860f
Renan Jefferson Rodrigues Silva <renanjefferson.silva@gmail.com>
2020-07-03 11:29:38 -0300

    feat: Listagem de produtos dinâmica

--- js/App.js
+++ js/App.js
@@ -47,8 +47,35 @@ Primise()
     console.warn(error);
   });
 
+function formatName(name, limit) {
+  return name.slice(0, limit) + (name.length > limit ? "..." : "");
+}
+
+function formatValue(value) {
+  return Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(value);
+}
+
 /**
  * List products
  */
 
-function listProduct(products) {}
+function listProduct(products) {
+  var ul = document.querySelector(".products");
+  products.map(
+    (product) =>
+      (ul.innerHTML += `<li class="product">
+  <a href="#"
+    ><img
+      src="${product.image}"
+      alt="${product.name}"
+  /></a>
+  <div class="product-inf">
+    <h3 class="product-title">${formatName(product.name, 18)}</h3>
+    <p class="product-price">${formatValue(product.price)}</p>
+  </div>
+</li>`)
+  );
+}
