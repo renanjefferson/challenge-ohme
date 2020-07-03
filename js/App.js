402cb4ceaf6e7a4e34fb472dcee78cd055dc1192
Renan Jefferson Rodrigues Silva <renanjefferson.silva@gmail.com>
2020-07-03 09:49:28 -0300

    first commit

---
+++ js/App.js
@@ -0,0 +1,54 @@
+/**
+ * Menu
+ */
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "90%";
+  document.getElementById("mySidenav").style.padding = "30px";
+  document.getElementById("background-opacity").style.display = "block";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.padding = "0";
+  document.getElementById("background-opacity").style.display = "none";
+}
+
+/**
+ * Get Mock Products
+ */
+
+const Primise = function () {
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.overrideMimeType("application/json");
+    xhr.open("GET", "./utils/mock-products.json", true);
+
+    xhr.send(null);
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          resolve(JSON.parse(xhr.response));
+        } else {
+          reject("Erro na requisição");
+        }
+      }
+    };
+  });
+};
+
+Primise()
+  .then(function (response) {
+    this.listProduct(response.products);
+  })
+  .catch(function (error) {
+    console.warn(error);
+  });
+
+/**
+ * List products
+ */
+
+function listProduct(products) {}
